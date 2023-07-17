8a63968da647550021833e7f0bab8a9573dd64e9
SardarkhanStella <skhan@stellatechnology.com>
2023-07-17 18:46:10 +0500

    Make a test pass by throwing exception from method.

--- app/src/main/java/com/raywenderlich/android/cocktails/game/viewmodel/model/Question.kt
+++ app/src/main/java/com/raywenderlich/android/cocktails/game/viewmodel/model/Question.kt
@@ -8,6 +8,8 @@ class Question(private val correctOption: String, val inCorrectOption: String) {
         private set
 
     fun answer(option: String): Boolean {
+        if(option!=correctOption && option!=inCorrectOption)
+            throw java.lang.IllegalArgumentException("Invalid Option")
         answeredOption = option
         return correctOption == answeredOption
     }
