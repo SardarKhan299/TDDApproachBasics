81912febdc0ab644e62a12437668d5e5faf5e1e8
SardarkhanStella <skhan@stellatechnology.com>
2023-07-17 16:10:18 +0500

    Make the test pass by writing the answered option in the answer function.

--- app/src/main/java/com/raywenderlich/android/cocktails/game/viewmodel/model/Question.kt
+++ app/src/main/java/com/raywenderlich/android/cocktails/game/viewmodel/model/Question.kt
@@ -1,6 +1,9 @@
 package com.raywenderlich.android.cocktails.game.viewmodel.model
 
 class Question(correctOption: String, inCorrectOption: String) {
+    fun answer(option: String) {
+        answeredOption = option
+    }
 
     var answeredOption: String? = null
         private set
