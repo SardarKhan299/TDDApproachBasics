3f371d5b92fc8803097f126bb5c9507a9acc89e7
SardarkhanStella <skhan@stellatechnology.com>
2023-07-17 15:24:03 +0500

    Make the test pass by writing the incrementSCore method and check the high score.

--- app/src/main/java/com/raywenderlich/android/cocktails/game/viewmodel/model/Game.kt
+++ app/src/main/java/com/raywenderlich/android/cocktails/game/viewmodel/model/Game.kt
@@ -1,12 +1,17 @@
 package com.raywenderlich.android.cocktails.game.viewmodel.model
 
 class Game {
-    fun incrementScore() {
-    }
 
     var score =0
         private set
     var hightScore = 0
         private set
+
+    fun incrementScore() {
+        score++
+        if(score>hightScore){
+            hightScore++
+        }
+    }
 
 }
