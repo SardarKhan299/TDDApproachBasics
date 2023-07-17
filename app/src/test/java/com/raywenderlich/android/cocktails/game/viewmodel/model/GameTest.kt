9fa95a8a4eeb627997fb51d1c10f0e0f89e22ba1
SardarkhanStella <skhan@stellatechnology.com>
2023-07-17 15:26:49 +0500

    Add test case for checking incrementing the score when increment score method called.

--- app/src/test/java/com/raywenderlich/android/cocktails/game/viewmodel/model/GameTest.kt
+++ app/src/test/java/com/raywenderlich/android/cocktails/game/viewmodel/model/GameTest.kt
@@ -1,9 +1,21 @@
 package com.raywenderlich.android.cocktails.game.viewmodel.model
 
+import org.junit.Assert
 import org.junit.Test
 
 
 class GameTest(){
+
+
+    @Test
+    fun whenIncrementingScoreShouldIncrementSCore(){
+        // Given
+        val game = Game()
+        // When
+        game.incrementScore()
+        // Then
+        Assert.assertEquals(1,game.score)
+    }
 
     @Test
     fun whenIncrementScoreShouldIncrementHighScore(){
