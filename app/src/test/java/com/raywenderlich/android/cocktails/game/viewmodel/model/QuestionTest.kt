7193708425b7080a9925251f305ef643c7c0515b
SardarkhanStella <skhan@stellatechnology.com>
2023-07-17 16:05:54 +0500

    Write the test for having answered option in question class and make it fail.

--- app/src/test/java/com/raywenderlich/android/cocktails/game/viewmodel/model/QuestionTest.kt
+++ app/src/test/java/com/raywenderlich/android/cocktails/game/viewmodel/model/QuestionTest.kt
@@ -1,6 +1,7 @@
 package com.raywenderlich.android.cocktails.game.viewmodel.model
 
 import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class QuestionTest {
@@ -9,6 +10,13 @@ class QuestionTest {
     fun whenCreatingQuestionShouldNotHaveAnsweredOption(){
         val question = Question("Correct","Incorrect")
         Assert.assertNull(question.answeredOption)
+    }
+
+    @Test
+    fun whenAnsweringQuestionShouldHaveAnsweredOption(){
+        val question = Question("CORRECT","INCORRECT")
+        question.answer("OPTION")
+        assertEquals("Answered Option is not There","OPTION",question.answeredOption)
     }
 
 }
