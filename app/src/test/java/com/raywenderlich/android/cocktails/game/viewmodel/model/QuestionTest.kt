e0e454157bf8d3e3aee194de72548a2dae77726f
SardarkhanStella <skhan@stellatechnology.com>
2023-07-17 18:59:05 +0500

    Refactor Unit Test by adding Before method for TestClass.

--- app/src/test/java/com/raywenderlich/android/cocktails/game/viewmodel/model/QuestionTest.kt
+++ app/src/test/java/com/raywenderlich/android/cocktails/game/viewmodel/model/QuestionTest.kt
@@ -2,40 +2,43 @@ package com.raywenderlich.android.cocktails.game.viewmodel.model
 
 import org.junit.Assert
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
 class QuestionTest {
 
+    private lateinit var question:Question
+
+    @Before
+    fun setup(){
+        question = Question("Correct","Incorrect")
+    }
+
     @Test
     fun whenCreatingQuestionShouldNotHaveAnsweredOption(){
-        val question = Question("Correct","Incorrect")
         Assert.assertNull(question.answeredOption)
     }
 
     @Test
     fun whenAnsweringQuestionShouldHaveAnsweredOption(){
-        val question = Question("CORRECT","INCORRECT")
         question.answer("OPTION")
         assertEquals("Answered Option is not There","OPTION",question.answeredOption)
     }
 
     @Test
     fun whenAnswerWithCorrectOptionShouldReturnTrue(){
-        val question = Question("CORRECT","INCORRECT")
         val result = question.answer("CORRECT")
         assertTrue("Option is incorrect",result)
     }
 
     @Test
     fun whenAnswerWithInCorrectOptionShouldReturnTrue(){
-        val question = Question("CORRECT","INCORRECT")
         val result = question.answer("INCORRECT")
         assertFalse("Option is correct",result)
     }
 
     @Test(expected = java.lang.IllegalArgumentException::class)
     fun whenAnswerWithInvalidOptionShouldThrowException(){
-        val question = Question("CORRECT","INCORRECT")
         val result = question.answer("Invalid")
     }
 
