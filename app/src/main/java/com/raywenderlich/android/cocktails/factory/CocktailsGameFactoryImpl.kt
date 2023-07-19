e08a73d0a4252859f290788c9758ba3706c53078
SardarkhanStella <skhan@stellatechnology.com>
2023-07-19 19:54:48 +0500

    Make Test pass by calling BuildGame Method and verify that getAlcoholic method will called.

--- app/src/main/java/com/raywenderlich/android/cocktails/factory/CocktailsGameFactoryImpl.kt
+++ app/src/main/java/com/raywenderlich/android/cocktails/factory/CocktailsGameFactoryImpl.kt
@@ -1,9 +1,20 @@
 package com.raywenderlich.android.cocktails.factory
 
+import com.raywenderlich.android.cocktails.common.network.Cocktail
 import com.raywenderlich.android.cocktails.common.repository.CocktailsRepository
+import com.raywenderlich.android.cocktails.common.repository.RepositoryCallback
 
 class CocktailsGameFactoryImpl (private val repository: CocktailsRepository): CocktailsGameFactory {
     override fun buildGame(callback: CocktailsGameFactory.Callback) {
+        repository.getAlcoholic(object : RepositoryCallback<List<Cocktail>,String>{
+            override fun onSuccess(t: List<Cocktail>) {
 
+            }
+
+            override fun onError(e: String) {
+
+            }
+
+        })
     }
 }
