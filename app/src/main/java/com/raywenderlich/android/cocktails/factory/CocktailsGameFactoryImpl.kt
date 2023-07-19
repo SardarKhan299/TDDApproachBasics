a7afcdff24d4987d1697fd3aae6bc501f06edd81
SardarkhanStella <skhan@stellatechnology.com>
2023-07-19 20:06:33 +0500

    Make the Test Pass by calling callback for onSuccess of the callback and verify it.

--- app/src/main/java/com/raywenderlich/android/cocktails/factory/CocktailsGameFactoryImpl.kt
+++ app/src/main/java/com/raywenderlich/android/cocktails/factory/CocktailsGameFactoryImpl.kt
@@ -3,12 +3,13 @@ package com.raywenderlich.android.cocktails.factory
 import com.raywenderlich.android.cocktails.common.network.Cocktail
 import com.raywenderlich.android.cocktails.common.repository.CocktailsRepository
 import com.raywenderlich.android.cocktails.common.repository.RepositoryCallback
+import com.raywenderlich.android.cocktails.game.viewmodel.model.Game
 
 class CocktailsGameFactoryImpl (private val repository: CocktailsRepository): CocktailsGameFactory {
     override fun buildGame(callback: CocktailsGameFactory.Callback) {
         repository.getAlcoholic(object : RepositoryCallback<List<Cocktail>,String>{
             override fun onSuccess(t: List<Cocktail>) {
-
+                callback.onSuccess(Game(emptyList()))
             }
 
             override fun onError(e: String) {
--- app/src/test/java/com/raywenderlich/android/cocktails/game/CocktailGameFactoryUnitTest.kt
+++ app/src/test/java/com/raywenderlich/android/cocktails/game/CocktailGameFactoryUnitTest.kt
@@ -1,17 +1,25 @@
 package com.raywenderlich.android.cocktails.game
 
+import com.raywenderlich.android.cocktails.common.network.Cocktail
 import com.raywenderlich.android.cocktails.common.repository.CocktailsRepository
+import com.raywenderlich.android.cocktails.common.repository.RepositoryCallback
 import com.raywenderlich.android.cocktails.factory.CocktailsGameFactory
 import com.raywenderlich.android.cocktails.factory.CocktailsGameFactoryImpl
 import org.junit.Before
 import org.junit.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
+import org.mockito.kotlin.*
 
 class CocktailGameFactoryUnitTest {
     private lateinit var repository: CocktailsRepository
     private lateinit var factory: CocktailsGameFactory
+
+    private val cocktails = listOf(
+        Cocktail("1", "Drink1", "image1"),
+        Cocktail("2", "Drink2", "image2"),
+        Cocktail("3", "Drink3", "image3"),
+        Cocktail("4", "Drink4", "image4")
+    )
+
 
     @Before
     fun setup(){
@@ -24,5 +32,22 @@ class CocktailGameFactoryUnitTest {
         factory.buildGame(mock())
         verify(repository).getAlcoholic(any())
     }
+
+    @Test
+    fun buildGameShouldCallOnSuccess(){
+        val callback = mock<CocktailsGameFactory.Callback>()
+        setupRepositoryWithCoctails(repository)
+        factory.buildGame(callback)
+        verify(callback).onSuccess(any())
+
+    }
+
+    private fun setupRepositoryWithCoctails(repository: CocktailsRepository) {
+        doAnswer {
+            val callback : RepositoryCallback<List<Cocktail>,String> = it.getArgument(0)
+            callback.onSuccess(cocktails)
+        }.whenever(repository).getAlcoholic(any())
+    }
+
 
 }
