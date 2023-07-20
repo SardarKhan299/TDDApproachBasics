48e4ce8c94651b5e5a0c239807fe8f662187176e
SardarkhanStella <skhan@stellatechnology.com>
2023-07-20 14:43:30 +0500

    make the test pass by building game with list of questions from cocktail list.

--- app/src/main/java/com/raywenderlich/android/cocktails/factory/CocktailsGameFactoryImpl.kt
+++ app/src/main/java/com/raywenderlich/android/cocktails/factory/CocktailsGameFactoryImpl.kt
@@ -4,14 +4,16 @@ import com.raywenderlich.android.cocktails.common.network.Cocktail
 import com.raywenderlich.android.cocktails.common.repository.CocktailsRepository
 import com.raywenderlich.android.cocktails.common.repository.RepositoryCallback
 import com.raywenderlich.android.cocktails.game.viewmodel.model.Game
+import com.raywenderlich.android.cocktails.game.viewmodel.model.Question
 import com.raywenderlich.android.cocktails.game.viewmodel.model.Score
 
 class CocktailsGameFactoryImpl (private val repository: CocktailsRepository): CocktailsGameFactory {
     override fun buildGame(callback: CocktailsGameFactory.Callback) {
         repository.getAlcoholic(object : RepositoryCallback<List<Cocktail>,String>{
-            override fun onSuccess(t: List<Cocktail>) {
+            override fun onSuccess(cocktaiList: List<Cocktail>) {
+                val questions = buildQuestions(cocktaiList)
                 val score = Score(repository.getHighScore())
-                val game = Game(emptyList(),score)
+                val game = Game(questions,score)
                 callback.onSuccess(game)
             }
 
@@ -20,5 +22,12 @@ class CocktailsGameFactoryImpl (private val repository: CocktailsRepository): Co
             }
 
         })
+    }
+
+    private fun buildQuestions(cocktaiList: List<Cocktail>): List<Question> {
+        return cocktaiList.map { coctail->
+            val otherCocktail = cocktaiList.shuffled().first{it != coctail}
+            Question(coctail.strDrink,otherCocktail.strDrink,coctail.strDrinkThumb)
+        }
     }
 }
