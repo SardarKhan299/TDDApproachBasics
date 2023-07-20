905c02d30d09a717766469ee7e2da46a14dd2394
SardarkhanStella <skhan@stellatechnology.com>
2023-07-20 15:37:18 +0500

    Add ViewModel and add build game from viewModel that call Factory

---
+++ app/src/main/java/com/raywenderlich/android/cocktails/game/viewmodel/CocktailsGameViewModel.kt
@@ -0,0 +1,33 @@
+package com.raywenderlich.android.cocktails.game.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.raywenderlich.android.cocktails.common.repository.CocktailsRepository
+import com.raywenderlich.android.cocktails.factory.CocktailsGameFactory
+import com.raywenderlich.android.cocktails.game.viewmodel.model.Game
+import com.raywenderlich.android.cocktails.game.viewmodel.model.Question
+import com.raywenderlich.android.cocktails.game.viewmodel.model.Score
+
+class CocktailsGameViewModel(val repository: CocktailsRepository, val factory: CocktailsGameFactory):ViewModel() {
+
+    private val loadingLiveData = MutableLiveData<Boolean>()
+    private val errorLiveData = MutableLiveData<Boolean>()
+    private val questionLiveData = MutableLiveData<Question>()
+    private val scoreLiveData = MutableLiveData<Score>()
+    fun getLoading(): LiveData<Boolean> = loadingLiveData
+    fun getError(): LiveData<Boolean> = errorLiveData
+    fun getQuestion(): LiveData<Question> = questionLiveData
+    fun getScore(): LiveData<Score> = scoreLiveData
+    fun initGame() {
+        factory.buildGame(object : CocktailsGameFactory.Callback{
+            override fun onSuccess(game: Game) {
+            }
+
+            override fun onError() {
+            }
+
+        })
+    }
+
+}
---
+++ app/src/test/java/com/raywenderlich/android/cocktails/game/viewmodel/model/CocktailGameViewModelUnitTest.kt
@@ -0,0 +1,65 @@
+package com.raywenderlich.android.cocktails.game.viewmodel.model
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.raywenderlich.android.cocktails.common.repository.CocktailsRepository
+import com.raywenderlich.android.cocktails.factory.CocktailsGameFactory
+import com.raywenderlich.android.cocktails.game.viewmodel.CocktailsGameViewModel
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.*
+
+class CocktailGameViewModelUnitTest {
+
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+    private lateinit var repository: CocktailsRepository
+    private lateinit var factory: CocktailsGameFactory
+    private lateinit var viewModel: CocktailsGameViewModel
+    private lateinit var game: Game
+    private lateinit var loadingObserver: Observer<Boolean>
+    private lateinit var errorObserver: Observer<Boolean>
+    private lateinit var scoreObserver: Observer<Score>
+    private lateinit var questionObserver: Observer<Question>
+
+    @Before
+    fun setup() {
+        // 1
+        repository = mock()
+        factory = mock()
+        viewModel = CocktailsGameViewModel(repository, factory)
+        // 2
+        game = mock()
+        // 3
+        loadingObserver = mock()
+        errorObserver = mock()
+        scoreObserver = mock()
+        questionObserver = mock()
+        viewModel.getLoading().observeForever(loadingObserver)
+        viewModel.getScore().observeForever(scoreObserver)
+        viewModel.getQuestion().observeForever(questionObserver)
+        viewModel.getError().observeForever(errorObserver)
+    }
+
+    @Test
+    fun initShouldBuildGame(){
+        viewModel.initGame()
+        verify(factory).buildGame(any())
+    }
+
+    private fun setUpFactoryWithSuccessGame(game:Game){
+        doAnswer {
+            val callback:CocktailsGameFactory.Callback = it.getArgument(0)
+            callback.onSuccess(game)
+        }.whenever(factory).buildGame(any())
+    }
+
+    private fun setUpFactoryWithError(){
+        doAnswer {
+            val callback:CocktailsGameFactory.Callback = it.getArgument(0)
+            callback.onError()
+        }.whenever(factory).buildGame(any())
+    }
+
+}
